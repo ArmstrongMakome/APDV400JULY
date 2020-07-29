072a6825c05f91ff404be476b616b7a316f77aa1
ArmstrongMakome <62277724+ArmstrongMakome@users.noreply.github.com>
2020-07-29 15:31:59 +0200

    Update Calculator201601028.java

--- Calculator201601028.java
+++ Calculator201601028.java
@@ -15,7 +15,7 @@ public class Calculator201601028 {
         add=num1+num2;
         subtract=num1-num2;
         multiply=num1*num2;
-        divide=num1*num2;
+        divide=num1/num2;
         
         
         
